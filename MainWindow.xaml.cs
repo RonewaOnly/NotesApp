ae5d49883d8ea89234a970276b67b05da1786902
RonewaOnly <125514494+RonewaOnly@users.noreply.github.com>
2023-07-18 19:43:30 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NotesApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		List<NoteTaken> noted = new List<NoteTaken>();
+		public string title="", comments = "";
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void btnClicked(object sender, RoutedEventArgs e)
+		{
+			if(NoteTitle.Text == string.Empty || CommentFill.Text == string.Empty)
+			{
+				MessageBox.Show("Fill in please", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+			}
+			else
+			{
+				takeNotes();
+			}
+			NoteTitle.Text = string.Empty;
+			CommentFill.Text = string.Empty;
+		}
+
+		public void takeNotes()
+		{
+			title = NoteTitle.Text;
+			comments = CommentFill.Text;
+			noted.Add(new NoteTaken(title, comments));
+			DateTime dateTime = DateTime.Now;
+
+			foreach (var i in noted)
+			{
+				NoteSaved.Items.Add(i.noteTitle);
+				NoteSaved.Items.Add(i.noteDescription);
+				NoteSaved.Items.Add(dateTime);
+			}
+			noted.Clear();
+			//NoteSaved.Items.Add(noted);
+		}
+	}
+}
---
+++ NoteTaken.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotesApp
+{
+	public class NoteTaken
+	{
+		public string noteTitle { get; set; }
+		public string noteDescription { get; set; }
+
+		public NoteTaken() { }
+		public NoteTaken(string noteTitle, string noteDescription)
+		{
+			this.noteTitle = noteTitle;
+			this.noteDescription = noteDescription;
+		}
+	}
+}
